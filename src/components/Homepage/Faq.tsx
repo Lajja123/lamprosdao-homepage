ae956e5051d524c97c21d2d7261b2c8f2aaa8b09
Lajja123 <lajja.lamprostech@gmail.com>
2025-10-10 11:53:26 +0530

    update arrow direction of faq section

--- src/components/Homepage/Faq.tsx
+++ src/components/Homepage/Faq.tsx
@@ -320,7 +320,7 @@ export default function Faq() {
             onClick={handleNext}
           >
             <Arrow
-              direction={left}
+              direction={right}
               size={70}
               className="sm:w-[60px] sm:h-[60px] md:w-[65px] md:h-[65px] lg:w-[70px] lg:h-[70px]"
             />{" "}
