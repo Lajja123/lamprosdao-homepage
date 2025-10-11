a06e867997f05cfe537c50e1b1d1f1a4a04eca8d
Lajja123 <lajja.lamprostech@gmail.com>
2025-10-11 17:52:23 +0530

    replace contribution meta-img

--- src/app/contribution/page.tsx
+++ src/app/contribution/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
     siteName: "Lampros DAO",
     images: [
       {
-        url: "https://lamprosdao.vercel.app/OGImages/contribution.png",
+        url: "https://lamprosdao.vercel.app/OGImages/contributions.png",
         width: 1200,
         height: 630,
         alt: "Contribution Lampros DAO",
